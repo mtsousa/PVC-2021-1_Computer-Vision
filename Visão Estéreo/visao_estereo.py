c77b722881c53f78a3e2f718b0d7aee7f5ed2797
João Luiz M. Jr <57275035+Joao-luiz05@users.noreply.github.com>
2021-03-16 19:16:48 -0300

    Update visao_estereo.py

--- Visão Estéreo/visao_estereo.py
+++ Visão Estéreo/visao_estereo.py
@@ -1,3 +1,3 @@
 # Autores: Matheus Teixeira de Sousa (teixeira.sousa@aluno.unb.br)
-#          João Luiz Machado Júnior ()
+#          João Luiz Machado Júnior (180137158@aluno.unb.br)
 # Disciplina: Princípios de Visão Computacional
