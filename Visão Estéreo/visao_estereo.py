dac7842fd4571ddab998805dfa4c33c5d0e684b8
Matheus Teixeira <mtsousa14@gmail.com>
2021-03-18 23:16:54 -0300

    Adicionei o filtro WLS para a formação do mapa de disparidade.

--- Visão Estéreo/visao_estereo.py
+++ Visão Estéreo/visao_estereo.py
@@ -4,12 +4,46 @@
 
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 
-imgL = cv.imread('im0.png',0)
-imgR = cv.imread('im1.png',0)
-stereo = cv.StereoBM_create(numDisparities=640, blockSize=15)
-#stereo = cv.StereoSGBM_create(numDisparities=640, blockSize=15)
-disparity = stereo.compute(imgL,imgR)
-plt.imshow(disparity,'gray')
-plt.show()
+imgL = cv.imread('im0.png', cv.COLOR_BGR2GRAY)
+imgR = cv.imread('im1.png', cv.COLOR_BGR2GRAY)
+
+window_size = 3
+
+left_matcher = cv.StereoSGBM_create(
+    minDisparity=-1,
+    numDisparities=40*16,  # Numero maximo de disparidades (640 para essa imagem)
+    blockSize=window_size,
+    P1=8 * 3 * window_size,
+    P2=32 * 3 * window_size,
+    disp12MaxDiff=12,
+    uniquenessRatio=10,
+    speckleWindowSize=50,
+    speckleRange=32,
+    preFilterCap=63,
+    mode=cv.STEREO_SGBM_MODE_SGBM_3WAY
+)
+right_matcher = cv.ximgproc.createRightMatcher(left_matcher)
+
+# parâmetros do filtro
+lmbda = 80000
+sigma = 1.3
+visual_multiplier = 6
+
+wls_filter = cv.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+wls_filter.setLambda(lmbda)
+
+wls_filter.setSigmaColor(sigma)
+displ = left_matcher.compute(imgL, imgR) 
+dispr = right_matcher.compute(imgR, imgL)
+displ = np.int16(displ)
+dispr = np.int16(dispr)
+filteredImg = wls_filter.filter(displ, imgL, None, dispr)
+
+# Normaliza o filtro
+filteredImg = cv.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv.NORM_MINMAX)
+filteredImg = np.uint8(filteredImg)
+
+# cv.imshow('filtered', filteredImg) Corrigir o tamanho da janela de exibição
+cv.imwrite('filtered.jpg', filteredImg)
+cv.waitKey(0)
