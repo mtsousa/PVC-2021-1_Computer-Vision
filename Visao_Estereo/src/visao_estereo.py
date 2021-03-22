31a47dbc49e1061a948c4ed98a989b0c18a1ce9b
João Luiz M. Jr <57275035+Joao-luiz05@users.noreply.github.com>
2021-03-22 13:49:03 -0300

    Inclusão de funções para mapa de profundidade e coordenadas 3D
    Foi removida a extensão regex, não mais utilizada para a leitura dos arquivos txt com dados de calibração;
    
    Foram adicionadas as funções world_coordinates e image_depth, auto-explicativas;
    
    Foram configuradas as leituras do arquivo texto de calibração para obter valores relevantes (linhas 132 à 136)

--- Visao_Estereo/src/visao_estereo.py
+++ Visao_Estereo/src/visao_estereo.py
@@ -5,25 +5,73 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-import re
 import os.path
 
 def data_reader(file_name):
 # Função para leitura de dados de calibração em arquivo .txt
 
-    lines = []
-    data = []
+	lines = []
+	data = []
 
-    with open(file_name) as f:
-        lines = f.readlines() # Armazenar linhas do arquivo em lista
+	with open(file_name) as f:
+		lines = f.readlines()  # Armazenar linhas do arquivo em lista
 
-    for i in range(len(lines)): data.append(re.findall(r"[-+]?\d*\.\d+|\d+", lines[i]))
-    # Coleta linha-a-linha de números relevantes na lista
+	# Percorrer linhas separando termos relevantes para inserir na lista
+	for i in range(len(lines)):
+		clean_line = lines[i].replace('=', ' ').replace('[', '').replace(';', '').replace(']', '')
+		results = clean_line.split()
+		data.append(results)
 
-    del data[0][0] # Remoção dos números irrelevantes que identificam o nome de cada câmera
-    del data[1][0]
+	# Remover strings que identificam o nome de cada variavel
+	for i in range(len(lines)):
+		del data[i][0]
 
-    return data
+	return data
+
+def world_coordinates (img, calib_data):
+# Função que utiliza dados de calibração das câmeras utilizadas para obter coordenadas 
+# 3D dos pontos no espaço
+
+	cx = float(calib_data[0][2])
+	cy = float(calib_data[0][5])
+	f = float(calib_data[0][0])
+	bline = float(calib_data[3][0])
+	doff = float(calib_data[2][0])
+
+	Q = np.float32([[1, 0, 0, -cx],
+                    [0, 1, 0, -cy],
+                    [0, 0, 0, f], 
+                    [0, 0, -1/bline, doff/bline]])
+
+
+	# Utilizamos a matriz Q para realizar uma reprojeção, convertendo pixels com
+	# valor de disparidade na sua sequência correspondente de coordenadas [x, y, z]
+	world_coordinates = cv.reprojectImageTo3D(img, Q)
+	# print(world_coordinates)
+
+	pass
+
+def image_depth (img, calib_data):
+# Produz um mapa de profundidade, originalmente em milímetros mas normalizado para a escala 0 - 254 em preto e branco
+# para os objetos na imagem
+
+	f = float(calib_data[0][0])
+	bline = float(calib_data[3][0])
+	doff = float(calib_data[2][0])
+	
+	Z = bline * f / (np.array(img) + doff)
+
+	filtered_depth_image = cv.normalize(src=Z, dst=Z, beta=0, alpha=254, norm_type=cv.NORM_MINMAX)
+	filtered_depth_image = np.uint8(filtered_depth_image)
+	
+	cv.imshow('DepthMap', filtered_depth_image)
+	cv.imwrite(os.path.join(data[0],'DepthMap.jpg'), filtered_depth_image)
+
+	# Podemos mapear os valores da imagem de profundidade de volta para unidades em milímetros
+	# por um simples ajuste de escala:
+	# original = np.array((filtered_depth_image - minimo) / float(maximo))
+
+	pass
 
 def disparity_calculator(left_image, right_image, disparities_num, min_num):
 # Função que calcula mapa de disparidades dadas duas imagens estereo-retificadas
@@ -32,7 +80,7 @@ def disparity_calculator(left_image, right_image, disparities_num, min_num):
 
 	left_matcher = cv.StereoSGBM_create(
 	    minDisparity = min_num,
-	    numDisparities = disparities_num,  # Numero maximo de disparidades (640 para essa imagem)
+	    numDisparities = disparities_num,  # Numero maximo de disparidades (600 para essa imagem)
 	    blockSize = window_size,
 	    P1 = 8*3*window_size,
 	    P2 = 32*3*window_size,
@@ -68,9 +116,6 @@ def disparity_calculator(left_image, right_image, disparities_num, min_num):
 
 # -------------------------------------------------------------------------------
 
-calib_jade_data = []
-calib_table_data = []
-
 # Define o diretório anterior ao diretório do programa
 base = os.path.abspath(os.path.dirname(__file__))
 base_new = base.replace('\\src', '')
@@ -83,8 +128,12 @@ data = [os.path.join(base_new, 'data', 'Middlebury', 'Jadeplant-perfect'),
 
 imgL = cv.imread(os.path.join(data[0], images[0]), cv.COLOR_BGR2GRAY)
 imgR = cv.imread(os.path.join(data[0], images[1]), cv.COLOR_BGR2GRAY)
-disparities_num = 600
-min_disp = 27
+
+calib_jade_data = data_reader(os.path.join(data[0], 'calib.txt'))
+calib_table_data = data_reader(os.path.join(data[1], 'calib.txt'))
+
+disparities_num = int(calib_jade_data[9][0])
+min_disp = int(calib_jade_data[8][0])
 
 filteredImg = disparity_calculator(imgL, imgR, disparities_num, min_disp)
 
@@ -99,9 +148,7 @@ plt.colorbar()
 plt.savefig(os.path.join(data[0], 'color_filtered.jpg'))
 plt.show()
 
+world_coordinates(filteredImg, calib_jade_data)
+image_depth(filteredImg, calib_jade_data)
+
 cv.waitKey(0)
-
-#calib_jade_data = data_reader('calib.txt')
-#calib_table_data = data_reader('calib_table.txt')
-
-#print(calib_jade_data)
